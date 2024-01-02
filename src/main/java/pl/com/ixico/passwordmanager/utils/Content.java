55f7464192127dc8bb9bd0253235d99eaf6432fe
emuchoro <01159054@pw.edu.pl>
2024-01-02 18:22:14 +0100

    help content changes

--- src/main/java/pl/com/ixico/passwordmanager/utils/Content.java
+++ src/main/java/pl/com/ixico/passwordmanager/utils/Content.java
@@ -5,12 +5,13 @@ public class Content {
     public static String help() {
         return """
                 STEP 1
-                1. Use silent mode (right top corner) when you are in a public place.
-                2. Provide master password that fulfills defined requirements and make sure to remember it.
-                3. Remember your checksum and verify it every time you provide master password to make sure it is correct.
+                1. Provide strong master password that fulfills the length requirement and make sure to remember it.
+                2. Remember your checksum and verify it every time you provide master password to make sure it is correct.
+                3. Make sure to use silent mode (right top corner) when you are in a public place.
                                 
                 STEP 2
                 4. Enter service name to generate password for authentication process.
+                5. Once you hit generate button, your password will be copied into your clipboard.
                 5. Make sure to refresh your session if you are still using the app (you will be logged out since it expires).
                 """;
     }
