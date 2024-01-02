60552802ef99d985c2cc6539a41e4903f4f2b8f8
emuchoro <01159054@pw.edu.pl>
2024-01-02 18:27:00 +0100

    Additional help hints

--- src/main/java/pl/com/ixico/passwordmanager/utils/Content.java
+++ src/main/java/pl/com/ixico/passwordmanager/utils/Content.java
@@ -7,12 +7,13 @@ public class Content {
                 STEP 1
                 1. Provide strong master password that fulfills the length requirement and make sure to remember it.
                 2. Remember your checksum and verify it every time you provide master password to make sure it is correct.
-                3. Make sure to use silent mode (right top corner) when you are in a public place.
+                3. Press generate button and wait for your key to be generated.
+                4. Make sure to use silent mode (right top corner) when you are in a public place.
                                 
                 STEP 2
-                4. Enter service name to generate password for authentication process.
-                5. Once you hit generate button, your password will be copied into your clipboard.
-                6. Make sure to refresh your session if you are still using the app (you will be logged out since it expires).
+                5. Enter service name to generate password for authentication process.
+                6. Once you hit generate button, your password will be copied into your clipboard.
+                7. Make sure to refresh your session if you are still using the app (you will be logged out since it expires).
                 """;
     }
 
